dcc33f3df51e1a43dba5a4db358384231e813c8c
Hugo Virgen <virgenherrera@gmail.com>
2017-11-07 09:36:10 -0600

    handle error connection to mongodb

--- src/Sys/mongodbConnection.ts
+++ src/Sys/mongodbConnection.ts
@@ -1,3 +1,4 @@
+import Debug from './Debug';
 import * as mongoose from 'mongoose';
 
 type connection = {
@@ -10,6 +11,16 @@ export default async function connectMongo():Promise<string>{
 	const ConnectUri = `${process.env.MONGODB_URI}/${process.env.MONGODB_DATABASE}`;
 
 	(<any>mongoose).Promise	= global.Promise;
-	const conn:connection	= await mongoose.connect(ConnectUri, { useMongoClient: true });
+	let conn:any;
+
+	try {
+		conn = await mongoose.connect(ConnectUri, { useMongoClient: true });
+	} catch ({name,message}) {
+
+		console.error(`${name}: ${message}`);
+		console.info('please update your connection settings in .env file and make your sure your mongodb service is running');
+		process.exit(1)
+	}
+
 	return `${"\n"}Using: "${conn.name}" Database in ${conn.host}:${conn.port} MongoDB`;
 }
