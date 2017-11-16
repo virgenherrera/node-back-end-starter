7d9449e43bbc34a85a7f10db64f4706e841002c5
Hugo Virgen <virgenherrera@gmail.com>
2017-11-16 12:47:17 -0600

    update param parser in createController task

--- tasks/createController.js
+++ tasks/createController.js
@@ -7,17 +7,18 @@ const {
 const { join }			= require('path');
 const getLastCliArg		= require('./lib/getLastCliArg');
 const ucfirst			= require('./lib/ucfirst');
-const controllerName		= getLastCliArg() || "controller" ;
+const parseCliArgs		= require("./lib/parseCliArgs");
 
 return (()=>{
+	let {name=null} 		= parseCliArgs();
 	const lowerRegEx		= new RegExp("{{module}}","g");
 	const CamelRegEx		= new RegExp("{{Module}}","g");
-	const CamelName			= ucfirst( controllerName );
+	const CamelName			= ucfirst( name );
 	const origin			= join( __dirname , '../examples/Controller.example' );
 	const destiny			= join(__dirname, `../src/Controller/${CamelName}.ts`);
 	const fileContent		= readFileSync(origin,'utf-8');
-	const newContent		= fileContent.toString().replace(lowerRegEx, controllerName).replace(CamelRegEx,CamelName);
-	const exportHandler		= `export { default as ${controllerName} }		from './Restful/${controllerName}';${"\n"}`;
+	const newContent		= fileContent.toString().replace(lowerRegEx, name).replace(CamelRegEx,CamelName);
+	const exportHandler		= `export { default as ${name} }		from './Restful/${name}';${"\n"}`;
 
 	if( existsSync( destiny ) ){
 		console.error(`Cannot Overwrite!${"\n"}Handler:	${destiny}${"\n"}Already Exists`);
