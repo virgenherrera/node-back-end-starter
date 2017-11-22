7c4bec45ac86a2d15de276f7e21d31c82d3f8edf
Hugo Virgen <virgenherrera@gmail.com>
2017-11-22 14:17:42 -0600

    update interfaces

--- src/Sys/interfaces.ts
+++ src/Sys/interfaces.ts
@@ -9,29 +9,29 @@ export interface iHandler{
 
 export interface iRestHandler extends iHandler{
 	readonly controller:object;
-	getAllHandler(...args:any[])	:any;
-	getOneHandler(...args:any[])	:any;
-	postHandler(...args:any[])		:any;
-	putHandler(...args:any[])		:any;
-	deleteHandler(...args:any[])	:any;
+	getAllHandler?(...args:any[])	:any;
+	getOneHandler?(...args:any[])	:any;
+	postHandler?(...args:any[])		:any;
+	putHandler?(...args:any[])		:any;
+	deleteHandler?(...args:any[])	:any;
 }
 
 export interface iController{
 	readonly repository	:object;
-	createAction	(p:object):Promise<any>;
-	listAction		(p:object):Promise<any>;
-	showAction		(p:object):Promise<any>;
-	editAction		(p:object):Promise<any>;
-	deleteAction	(p:object):Promise<any>;
+	createAction?	(p:object):Promise<any>;
+	listAction?		(p:object):Promise<any>;
+	showAction?		(p:object):Promise<any>;
+	editAction?		(p:object):Promise<any>;
+	deleteAction?	(p:object):Promise<any>;
 }
 
 export interface iRepository{
-	GetById(p:object):Promise<object>;
+	GetById?(p:object):Promise<object>;
 	FindOne?(p:object):Promise<object>;
-	GetAll(p:object):Promise<object>;
-	Create(p:object):Promise<object>;
-	Update(p:object):Promise<object>;
-	Delete(p:object):Promise<object>;
+	GetAll?(p:object):Promise<object>;
+	Create?(p:object):Promise<object>;
+	Update?(p:object):Promise<object>;
+	Delete?(p:object):Promise<object>;
 }
 
 export interface iResDto{
