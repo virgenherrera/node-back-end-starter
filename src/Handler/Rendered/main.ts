3392d2ac981748867fd5a25731a6d07f4c7647b5
Hugo Virgen <virgenherrera@gmail.com>
2017-12-07 09:03:36 -0600

    remove unwanted axios

--- src/Handler/Rendered/main.ts
+++ src/Handler/Rendered/main.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { IHandler } from '../../Sys/interfaces';
-import HandlerUtility from '../../Sys/HandlerUtility';
+// import HandlerUtility from '../../Sys/HandlerUtility';
 // only for debugging
 // import { dd } from '../../Sys/Debug';
 
@@ -21,10 +21,6 @@ class MainHandler implements IHandler {
 		// Attach handlers to express Router
 		this.router
 		.get('/', this.mainView.bind(this));
-	}
-
-	get handlerUtility( ): HandlerUtility {
-		return new HandlerUtility;
 	}
 
 	mainView( req: Request, res: Response, next: NextFunction ): any {
